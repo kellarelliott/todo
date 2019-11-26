9eeed9ce290ee751a63e2a07c7cf864c5a8306b3
Kellar Elliott <kellarelliott@gmail.com>
2019-11-26 18:57:03 +0000

    Polish

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,62 +1,60 @@
-<script>
-  $(function() {
+$(function () {
+
+  function taskHtml(task) {
+    var checkedStatus = task.done ? "checked" : "";
+    var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+      " data-id='" + task.id + "'" +
+      checkedStatus +
+      '><label>' +
+      task.title +
+      '</label></div></li>';
+
+    return liElement;
+  }
 
 
-    function taskHtml(task) {
-      var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
-        " data-id='" + task.id + "'" +
-        checkedStatus +
-        '><label>' +
-        task.title +
-        '</label></div></li>';
+  function toggleTask(e) {
+    var itemId = $(e.target).data("id");
 
-      return liElement;
-    }
+    var doneValue = Boolean($(e.target).is(':checked'));
 
-    function toggleTask(e) {
-      var itemId = $(e.target).data("id");
+    $.post("/tasks/" + itemId, {
+      _method: "PUT",
+      task: {
+        done: doneValue
+      }
+    });
+  }
 
+  $.get("/tasks").success(function (data) {
+    var htmlString = "";
 
-var doneValue = Boolean($(e.target).is(':checked'));
+    $.each(data, function (index, task) {
+      htmlString += taskHtml(task);
+    });
+    var ulTodos = $('.todo-list');
+    ulTodos.html(htmlString);
 
+    $('.toggle').change(toggleTask);
 
-      $.post("/tasks/" + itemId, {
-    _method: "PUT",
-        task: {
-    done: doneValue
-}
-});
-}
-    $.get("/tasks").success( function( data ) {
-      var htmlString = "";
-
-      $.each(data, function(index,  task) {
+  });
 
 
-    htmlString += taskHtml(task);
-});
-var ulTodos = $('.todo-list');
-ulTodos.html(htmlString);
-
-$('.toggle').change(toggleTask);
-
-});
-
-    $('#new-form').submit(function(event) {
+  $('#new-form').submit(function (event) {
     event.preventDefault();
-var textbox = $('.new-todo');
-      var payload = {
-    task: {
-    title: textbox.val()
-}
-};
-      $.post("/tasks", payload).success(function(data) {
-        var htmlString = taskHtml(data);
-var ulTodos = $('.todo-list');
-ulTodos.append(htmlString);
-$('.toggle').click(toggleTask);
+    var textbox = $('.new-todo');
+    var payload = {
+      task: {
+        title: textbox.val()
+      }
+    };
+    $.post("/tasks", payload).success(function (data) {
+      var htmlString = taskHtml(data);
+      var ulTodos = $('.todo-list');
+      ulTodos.append(htmlString);
+      $('.toggle').click(toggleTask);
+      $('.new-todo').val('');
+    });
+  });
+
 });
-});
-});
-</script>
